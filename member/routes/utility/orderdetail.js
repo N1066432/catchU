5e7d011d0e5b3091c84249bd4a6e7ca353e79a51
n1066442 <n1066442@ntub.edu.tw>
2020-08-17 13:52:34 +0800

    updata orderdetail &logout

--- member/routes/utility/orderdetail.js
+++ member/routes/utility/orderdetail.js
@@ -24,6 +24,29 @@ var list = async function(){
 }
 
 //------------------------------------------
+//執行資料庫動作的函式-取出單一員工
+//------------------------------------------
+var list = async function(memberphone,ordtime){
+    var result={};
+    
+    await sql('SELECT * FROM orderdetail WHERE memberphone = $1 and ordtime = $1', [memberphone,ordtime])
+    .then((data) => {
+        console.log(data.rows.length)
+        console.log(data.rows)
+
+        if(data.rows.length > 0){
+            result = data.rows;   
+        }else{
+            result = -1;
+        }    
+    }, (error) => {
+        result = null;
+    });
+    
+    return result;
+}
+
+//------------------------------------------
 //執行資料庫動作的函式-新增會員資料
 //------------------------------------------
 
