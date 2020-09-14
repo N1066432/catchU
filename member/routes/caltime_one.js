c97fc59dfb8bbab808a9f92718c2bcd4f1a93b0c
n1066442 <n1066442@ntub.edu.tw>
2020-09-14 21:59:04 +0800

    add endtime 9/14

---
+++ member/routes/caltime_one.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+//增加引用函式
+var moment = require('moment');
+const caltime = require('./utility/caltime');
+
+//接收GET請求
+router.get('/', function(req, res, next) {
+    var memberphone = req.session.memberphone;
+    //console.log(memberphone)
+
+    caltime.query(memberphone).then(data => {
+        //console.log(data)
+        if(data==null){
+            res.render('error');  //導向錯誤頁面
+        }else if(data.length > 0){
+            res.render('caltime_addend_form', {items:data});  //將資料傳給顯示頁面
+        }else{
+            res.render('notFound');  //導向找不到頁面
+        }  
+    })
+});
+
+module.exports = router;
