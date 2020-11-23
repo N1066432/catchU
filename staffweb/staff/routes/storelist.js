090483978ae934cc12efdece59633933e53c72e2
Oscar-Zhou <n1066418@ntub.edu.tw>
2020-11-23 23:39:09 +0800

    modify final vision2

--- staffweb/staff/routes/storelist.js
+++ staffweb/staff/routes/storelist.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
         }else if(data.length > 0){
             res.render('storelist', {items:data});  //將資料傳給顯示頁面
         }else{
-            res.render('notFound');  //導向找不到頁面
+            res.render('storenotFound');  //導向找不到頁面
         }  
     })
 });
